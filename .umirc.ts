7893af0ab58634f638ce1ab741bafbbe5cadf5ba
徐航 <283648394@qq.com>
2022-08-09 08:55:21 +0800

    修改logo

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,23 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'Misro',
-  favicon: 'logo',
-  logo: '',
+  favicon: '/logo.png',
+  logo: '/logo.png',
   mode: 'site',
+  publicPath: './',
+  history: {
+    type: 'hash'
+  },
+  locales: [['zh-CN', '中文']],
+  outputPath: 'docs-dist',
+  navs: [
+    {
+      title: '开始',
+      path: '/getting-started'
+    }
+  ],
+  sitemap: {
+    hostname: 'www.ucunn.cn'
+  }
   // more config: https://d.umijs.org/config
 });
